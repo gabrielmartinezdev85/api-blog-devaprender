246f0d8214a835f0a8989b5863de01f5e17a01fe
gabrielmartinezdev85 <gabriel.martinezpp@yahoo.com.br>
2023-07-20 10:23:07 -0300

    Versao_Railway_2

--- estrutura_banco_de_dados.py
+++ estrutura_banco_de_dados.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 # Criar um instância de SQLAlchemy
 app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Uc0BGuqU0geFz3xB3Xp4@containers-us-west-164.railway.app:7729/railway'
+app.config['SQLALCHEMY_PGDATABASE'] = 'railway'
+app.config['SQLALCHEMY_PGHOST'] = 'containers-us-west-164.railway.app'
+app.config['SQLALCHEMY_PGPASSWORD'] = 'PGPASSWORD=Uc0BGuqU0geFz3xB3Xp4 psql -h containers-us-west-164.railway.app -U postgres -p 7729 -d railway'
+app.config['SQLALCHEMY_PGPORT'] = '7729'
+app.config['SQLALCHEMY_PGUSER'] = 'postgres'
 
 db = SQLAlchemy(app)
 db: SQLAlchemy
 
 # Definir a estrutra da tabela Postagem: id_postagem, titulo, autor
-
 
 class Postagem(db.Model):
     __tablename__ = 'postagem'
@@ -19,7 +23,6 @@ class Postagem(db.Model):
     titulo = db.Column(db.String)
     id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor'))
 # Definir a estrutra da tabela Autor: id_autor, nome, email, senha, admin, postagens
-
 
 class Autor(db.Model):
     __tablename__ = 'autor'
@@ -33,12 +36,12 @@ class Autor(db.Model):
 
 def inicializar_banco():
     with app.app_context():
-    # Executar o comando para criar o banco de dados
+        # Executar o comando para criar o banco de dados
         db.drop_all()
         db.create_all()
         # Criar usuários adminstradores
         autor = Autor(nome='gabriel', email='gabriel.martinezpp@yahoo.com.br',
-                    senha='123456', admin=True)
+                      senha='123456', admin=True)
         db.session.add(autor)
         db.session.commit()
 
